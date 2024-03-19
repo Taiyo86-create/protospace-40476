7e8c69449249d93ef8375ed341529b25336feb61
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-19 22:52:47 +0900

    ログアウト状態で編集ページに移行できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user).all
