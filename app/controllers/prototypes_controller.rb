8b464849719e64fbc54d50b3e49fa85202055097
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-20 17:07:53 +0900

    編集に失敗した時内容は消え内容修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -36,6 +36,7 @@ class PrototypesController < ApplicationController
 
   def update
     prototype = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
       redirect_to action: :show
     else
